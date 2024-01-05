5e89876b371204c287318446e892460e71a3a719
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-05 19:26:34 +0100

    add: Addbutton functionality

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,14 +1,15 @@
 import { ShopType } from "@/types/componentTypes";
 import { CartItem, Product } from "@/types/dataTypes";
-import React, { FC, SyntheticEvent, useState } from "react";
+import React, { FC, SyntheticEvent, useEffect, useState } from "react";
 import useSWR from "swr";
 
 const url =
   "https://billionnaire-shopping-cart-backend-e3uzzxqbva-uc.a.run.app/api/";
 
 const Shop: FC<ShopType> = ({ billionnaire }) => {
-  const [cart, setCart] = useState<CartItem[]>([]);
-
+    const [cart, setCart] = useState<CartItem[]>([]);
+    const [sumCost, setSumCost] = useState<number>(0);
+    
   const productsFetcher = (args: string) =>
     fetch(args).then((response) => response.json());
   const {
@@ -21,14 +22,53 @@ const Shop: FC<ShopType> = ({ billionnaire }) => {
     const target = evt.target as HTMLButtonElement;
     const id = parseInt(target.id.slice(-1));
     const item = cart.find(x => x.id === id)
+
     if(item === undefined) {
         setCart([...cart, {id: id, item: products[id], qty: 1}])
+        return
     }
+
+    const index = cart.indexOf(item)
+    const newArray = [...cart];
+    newArray.splice(index, 2, {id: id, item: products[id], qty: ++item.qty} )
+
+    setCart(newArray);
   };
 
-  const subtractItemHandler = () => {};
+  const subtractItemHandler = (evt: SyntheticEvent) => {
+    const target = evt.target as HTMLButtonElement;
+    const id = parseInt(target.id.slice(-1));
+    const item = cart.find(x => x.id === id)
 
-  if (error) return <p>Oh no Something wet wrong!</p>;
+    if(item === undefined) {
+        return
+    }
+
+    const index = cart.indexOf(item)
+
+    if(item.qty === 1) {
+        const newArray = cart.splice(index,2 )
+
+    setCart(newArray);
+    return
+    }
+    const newArray = [...cart];
+    cart.splice(index, 2, {id: id, item: products[id], qty: item.qty--} )
+
+    setCart(newArray);
+  };
+
+  
+  useEffect(() => {
+    if(cart.length){
+        const prices = cart.map(item => {
+            return item.item.price * item.qty
+        })
+        setSumCost(prices.reduce((a,b) => a + b));
+    }
+  },[cart]) 
+
+  if (error) return <p>Oh no Something went wrong!</p>;
   if (isLoading) return <p>Loading...</p>;
   return (
     <>
@@ -80,10 +120,11 @@ const Shop: FC<ShopType> = ({ billionnaire }) => {
         })}
       </div>
       <h2 className="my-16">Shopping Cart</h2>
+      <h2 className="py-2">Remaining Balance: {billionnaire.netWorth - sumCost} SEK</h2>
       {cart.map((cartItem: CartItem, i: number) => {
         return (
           <div className="my-2 bg-green-100 p-2 m-2 text-center" key={i}>
-            <h2>{cartItem.qty}x {cartItem.item.name}</h2>
+            <h2>{cartItem.qty}x {cartItem.item.name} = {cartItem.item.price * cartItem.qty} SEK</h2>
           </div>
         );
       })}
