9d3815de538674e36f0c47ebc81907a7369d2d8b
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-05 14:50:54 +0100

    add: IDE logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,11 @@ import { useEffect, useState } from 'react'
 import useSWR from 'swr';
 
 const url= "https://billionnaire-shopping-cart-backend-e3uzzxqbva-uc.a.run.app/api/"
+type Billionnaire = {
+name: string;
+netWorth: number;
+imagePath: string
+}
 
 export default function Home() {
   const billionnairesFetcher =(args: string) => fetch(args).then(response =>response.json())
@@ -14,11 +19,18 @@ export default function Home() {
   
   console.log(data)
   // console.log(products)
-  
-  return (
+  if(data) return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1>Home</h1>
+        {data.map((elm: Billionnaire, i: number) => {
+          return(
+            <div key={i}>
+              <h2>{elm.name}</h2>
+              <h2>{elm.netWorth}</h2>
+            </div>
+          )
+        })}
         {/* <h1>{products[0].Name}</h1> */}
       </div>
     </main>
