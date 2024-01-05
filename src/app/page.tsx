9af745d9608d3b0107baa01c4a10b832a6177a86
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-05 13:10:32 +0100

    edit: element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
         />
       </div>
 
-      <div>{data}</div>
+      <h1>{data}</h1>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
