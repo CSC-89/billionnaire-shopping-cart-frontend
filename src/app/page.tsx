371d1b31896869b41aea384fe2b5bf181816b34f
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-05 15:10:39 +0100

    add: routing for products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,24 +10,39 @@ netWorth: number;
 imagePath: string
 }
 
+type Product = {
+name: string;
+price: number;
+imagePath: string
+}
+
 export default function Home() {
   const billionnairesFetcher =(args: string) => fetch(args).then(response =>response.json())
   const productsFetcher =(args: string) => fetch(args).then(response =>response.json())
 
-  const { data } = useSWR(`${url}Billionnaires`, billionnairesFetcher );
-  // const { products } = useSWR(`${url}Products`, productsFetcher );
+  const { data: billionnaires } = useSWR(`${url}Billionnaires`, billionnairesFetcher );
+  const { data: products } = useSWR(`${url}Products`, productsFetcher );
   
-  console.log(data)
+  console.log(billionnaires)
+  console.log(products)
   // console.log(products)
-  if(data) return (
+  if(billionnaires && products) return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1>Home</h1>
-        {data.map((elm: Billionnaire, i: number) => {
+        {billionnaires.map((elm: Billionnaire, i: number) => {
           return(
             <div key={i}>
               <h2>{elm.name}</h2>
               <h2>{elm.netWorth}</h2>
+            </div>
+          )
+        })}
+        {products.map((elm: Product, i: number) => {
+          return(
+            <div key={i}>
+              <h2>{elm.name}</h2>
+              <h2>{elm.price}</h2>
             </div>
           )
         })}
